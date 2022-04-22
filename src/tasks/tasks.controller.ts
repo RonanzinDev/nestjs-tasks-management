99bb2164be21ebfeafd56cf4d6c005469a2e72dd
Ronanzindev <morellianogm@gmail.com>
2022-04-22 15:26:17 -0300

    Adding Get all taks method on the controller

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,7 +1,12 @@
 import { TasksService } from './tasks.service';
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private taskService: TasksService) {}
+
+  @Get()
+  getAllTask() {
+    return this.taskService.getAllTask();
+  }
 }
